d00ae6e8fb51a2d392bd55f1de788e5a62fed6e1
reisman <30975669+reisman@users.noreply.github.com>
2018-07-15 20:12:56 +0200

    Wrapping handlers for go

--- api/server.go
+++ api/server.go
@@ -4,7 +4,11 @@ import (
 	"GraphQLMessages/api/data"
 	"log"
 	"net/http"
+	"os"
 
+	"golang.org/x/net/http2"
+
+	"github.com/gorilla/handlers"
 	"github.com/graphql-go/graphql"
 	"github.com/graphql-go/handler"
 	"github.com/rs/cors"
@@ -17,14 +21,32 @@ func main() {
 	mux := http.NewServeMux()
 	graphqlHandler := createHandler(schema)
 	mux.Handle("/graphql", graphqlHandler)
-	corsHandler := cors.Default().Handler(mux)
-
-	port := ":8080"
-	log.Printf(`Starting GraphQL server on http://localhost%v`, port)
-	err := http.ListenAndServe(port, corsHandler)
+	handler := withCompression(withLogging(withCors(mux)))
+	
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: handler,
+	}
+	
+	log.Printf(`Starting GraphQL server on http://localhost%v`, server.Addr)
+	
+	http2.ConfigureServer(&server, &http2.Server{})
+	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatalf("Starting server failed: %v", err)
 	}
+}
+
+func withCors(handler http.Handler) http.Handler {
+	return cors.Default().Handler(handler)
+}
+
+func withCompression(handler http.Handler) http.Handler {
+	return handlers.CompressHandler(handler)
+}
+
+func withLogging(handler http.Handler) http.Handler {
+	return handlers.LoggingHandler(os.Stdout, handler)
 }
 
 func createHandler(schema graphql.Schema) *handler.Handler {
